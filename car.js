d1118f054b14d7be84a88d446ff35ddd29280219
Coderganesh <108095949+Ganeshshit@users.noreply.github.com>
2022-10-15 22:55:08 +0530

    Add files via upload

---
+++ car.js
@@ -0,0 +1,19 @@
+// var audio=document.createElement('audio');
+// audio.setAttribute('src','lamborgini.mp3');
+// audio.loop=true;
+// audio.play()
+// audiogo = new Audio('end.mp3');
+audio=new Audio('lamborgini.mp3')
+audio.play()
+// document.onkeydown = function (e) {
+//     console.log("Key code is ", e.keyCode)
+//     if (e.keyCode == 38) {
+//         dino = document.querySelector('.dino');
+//         dino.classList.add('animateDino');
+//         setTimeout(() => {
+//             dino.classList.remove('animateDino')
+//         }, 800);
+
+
+//     }
+// }
